2871a4b9a61d813ffd3399aabe9749feffab303c
Charlie Person <charlie.person@hyperisland.se>
2015-12-09 12:06:46 +0000

    attack.erb and attack_player_2_spec.rb added

---
+++ spec/features/attack_player_2_spec.rb
@@ -0,0 +1,13 @@
+
+feature 'Enter names' do
+
+  # As Player 1,
+  # So I can win a game of Battle,
+  # I want to attack Player 2, and I want to get a confirmation
+  scenario 'confirm player 2 has been attacked' do
+    sign_in_and_play
+    click_link('Attack!')
+    expect(page).to have_content 'Timmy Jones has been attacked by Johnny Cash!'
+  end
+
+end
