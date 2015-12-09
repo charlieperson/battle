ee26e6a2801b136db5972ddcbe8a8acfa74e5ae3
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2015-12-09 10:44:24 +0000

    Ch21 - defines method sign_in_and_play as web_helper

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -4,10 +4,7 @@ feature 'Enter names' do
   # So I can see how close I am to winning
   # I want to see Player 2's Hit Points
   scenario 'allows players to see eachothers hit points' do
-    visit '/'
-    fill_in(:player_1_name, with: 'Johnny Cash')
-    fill_in(:player_2_name, with: 'Timmy Jones')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content 'Johnny Cash: 60HP'
   end
 
